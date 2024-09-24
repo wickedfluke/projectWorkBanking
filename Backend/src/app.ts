8604a7daff990b1c8bd8d13aef4585a38e3665da
wickedfluke <gianbu00@gmail.com>
2024-09-24 15:29:34 +0200

    err for deploy

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -13,13 +13,17 @@ app.use(cors());
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
 
-app.use('/api', apiRouter);
 
 app.use(express.static(path.join(__dirname, 'public', 'frontand', 'browser')));
+
+
+app.use('/api', apiRouter);
+
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'frontand', 'browser', 'index.html'));
 });
+
 
 app.use(errorHandlers);
 
