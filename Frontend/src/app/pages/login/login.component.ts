3e9b079fcca874bbf99209405c3e23293bb92b6b
Dokjolly0 <alexviolattolibero.it@gmail.com>
2024-09-25 12:55:49 +0200

    IntermediateLoginFix

--- Frontend/src/app/pages/login/login.component.ts
+++ Frontend/src/app/pages/login/login.component.ts
@@ -1,38 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, NgForm, Validators } from '@angular/forms';
 import { getElementById, hideContent, showContent } from '../../functions/utils.html';
+import { debounceTime, delay, takeUntil, tap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
-  constructor(private authService: AuthService, private router: Router, private titleSrv: Title) {}
-  loginData: any = {
-    username: '',
-    password: '',
-  };
+export class LoginComponent implements OnInit, OnDestroy {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private titleSrv: Title,
+    private fb: FormBuilder
+  ) {}
+
+  loginForm = this.fb.group({
+    username: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+
   isPasswordVisible = false;
   passwordVisible: boolean = false;
   pageTitle = 'Login home banking';
 
+  private destroyed$ = new Subject<void>();
+
   ngOnInit(): void {
     this.titleSrv.setTitle(this.pageTitle);
-    this.startLoginTimeout();
+    this.initFormReset();
   }
 
-  startLoginTimeout() {
-    setTimeout(() => {
-      this.showCustomAlert();
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
 
-      setTimeout(() => {
-        window.location.reload();
-      }, 3000); 
-    }, 30000); 
+  private initFormReset() {
+    this.loginForm.valueChanges
+      .pipe(
+        debounceTime(30000),
+        takeUntil(this.destroyed$),
+        tap((_) => this.showCustomAlert()),
+        delay(2000),
+        tap((_) => this.hideCustomAlert())
+      )
+      .subscribe((_) => {
+        this.loginForm.reset({ username: '', password: '' }, { emitEvent: false });
+      });
   }
 
   showCustomAlert() {
@@ -49,9 +69,10 @@ export class LoginComponent {
     overlayElement.style.display = 'none';
   }
 
-
-  login(form: NgForm) {
-    this.authService.login(this.loginData.username, this.loginData.password).subscribe(
+  login() {
+    if (this.loginForm.invalid) return;
+    const { username, password } = this.loginForm.value;
+    this.authService.login(username!, password!).subscribe(
       () => {
         hideContent(getElementById('login-error'));
         this.router.navigate(['/dashboard']);
@@ -60,7 +81,7 @@ export class LoginComponent {
         const errorElement = getElementById('login-error');
         errorElement.innerText = err.error.message;
         if (err.error.message === 'username must be an email') {
-          errorElement.innerText = 'L\'username deve essere un indirizzo email.';
+          errorElement.innerText = "L'username deve essere un indirizzo email.";
         }
         showContent(errorElement);
       }
